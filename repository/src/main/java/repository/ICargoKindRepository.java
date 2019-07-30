9266908cf680db8c05ab8eade002fa38171645c8
Andrzejf <afornalik2@gmail.com>
2019-07-30 23:16:55 +0200

    add @Repository annotation.

--- repository/src/main/java/repository/ICargoKindRepository.java
+++ repository/src/main/java/repository/ICargoKindRepository.java
@@ -2,6 +2,9 @@ package repository;
 
 import entity.CargoKind;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface ICargoKindRepository extends JpaRepository<CargoKind,Long> {
 }
--- repository/src/main/java/repository/ICargoRepository.java
+++ repository/src/main/java/repository/ICargoRepository.java
@@ -2,6 +2,8 @@ package repository;
 
 import entity.Cargo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ICargoRepository extends JpaRepository<Cargo, Long> {
 }
